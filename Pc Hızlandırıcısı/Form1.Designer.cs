8657178bb665234e6692fe6492a3ed00ffe59e6d
Burak Can Kart <bucaka53.bck@gmail.com>
2022-09-17 14:00:00 +0300

    Açıklama

---
+++ Pc Hızlandırıcısı/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Pc_Hızlandırıcısı
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(248, 69);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(221, 116);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Basit Uygulamaları Temizle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(566, 69);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(221, 116);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Gelişmiş Sistem Uygulamalarını Temizle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 15000;
+            this.timer1.Tick += new System.EventHandler(this.Form1_Load);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Pc Hızlandırıcısı/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pc_Hızlandırıcısı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (var process in Process.GetProcessesByName("DTShellHlp"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("RadeonSoftware"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("amdow"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("AMDRSServ"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("SettingSyncHost"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("DiscSoftBusServiceLite"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("ApplicationFrameHost"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("HPSupportSolutionsFrameworkService"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("WUDFHost"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("java"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("WmiPrvSE"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("wlanext"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("armsvc"))
+            {
+                process.Kill();
+            }
+
+
+            /*BURDAN SONRASI ALERTO FALERTO
+             * KABATMA
+             * YİGENİM
+             * OKEE
+             * AYUKÜN MÜ HA
+             * AŞŞŞASI BUTON 2 BABBA
+            */
+
+            foreach (var process in Process.GetProcessesByName("OfficeClickToRun"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("SecurityHealthService"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("SystemSettingsBroker"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("StartMenuExperienceHost"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("dasHost"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("AppVShNotify"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("MoUsoCoreWorker"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("SppExtComObj"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("SearchIndexer"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("RuntimeBroker"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("UserOOBEBroker"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("ShellExperienceHost"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("notepad"))
+            {
+                process.Kill();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (var process in Process.GetProcessesByName("notepad"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("DTShellHlp"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("RadeonSoftware"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("amdow"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("AMDRSServ"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("SettingSyncHost"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("DiscSoftBusServiceLite"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("ApplicationFrameHost"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("HPSupportSolutionsFrameworkService"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("WUDFHost"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("java"))
+            {
+                process.Kill();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (var process in Process.GetProcessesByName("OfficeClickToRun"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("SecurityHealthService"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("SystemSettingsBroker"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("StartMenuExperienceHost"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("dasHost"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("AppVShNotify"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("MoUsoCoreWorker"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("SppExtComObj"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("SearchIndexer"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("RuntimeBroker"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("UserOOBEBroker"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("ShellExperienceHost"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("WmiPrvSE"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("wlanext"))
+            {
+                process.Kill();
+            }
+            foreach (var process in Process.GetProcessesByName("armsvc"))
+            {
+                process.Kill();
+            }
+        }
+    }
+}
